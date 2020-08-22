805bb84e0d3ca8d480b175b40d6f20e8b258b640
Konrad Kalita <konrad.kalita@openx.com>
2020-08-22 14:29:35 +0200

    feat: Setup apollo_server_express and implement CRUD operations on Post entity.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities';
+import 'reflect-metadata';
 import microConfig from './mikro-orm.config';
 import { __prod__ } from './constants';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver, PostResolver } from './resolvers';
 
 if (__prod__) {
   require('dotenv').config();
@@ -10,6 +14,23 @@ if (__prod__) {
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  });
 };
 
 main().catch(err => {
